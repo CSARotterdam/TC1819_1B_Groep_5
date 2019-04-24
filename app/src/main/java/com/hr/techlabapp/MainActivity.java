1ca34590838b5416f7d9d6207ffc84621037d696
Nolz <36479504+TehNolz@users.noreply.github.com>
2019-04-24 17:54:44 +0200

    Add 1st revision of login screen + networking

--- app/src/main/java/com/hr/techlabapp/MainActivity.java
+++ app/src/main/java/com/hr/techlabapp/MainActivity.java
@@ -1,30 +1,37 @@
 package com.hr.techlabapp;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "TL-MainActivity";
+    public Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.context = getApplicationContext();
 
-        Networking.thread.run();
+        Networking.thread.start();
     }
 
     public void loginButton(View view){
-        new Login().execute();
+        EditText usernameField = (EditText)findViewById(R.id.UsernameField);
+        String username = usernameField.getText().toString();
+        EditText passwordField = (EditText)findViewById((R.id.PasswordField));
+        String password = passwordField.getText().toString();
+        new Login().execute(username, password);
     }
 
 
@@ -32,15 +39,22 @@ public class MainActivity extends AppCompatActivity {
         private ProgressDialog dialog;
 
         protected void onPreExecute(){
-            dialog = new ProgressDialog(getApplicationContext());
+            dialog = new ProgressDialog(MainActivity.this);
             dialog.setMessage("Logging in...");
             dialog.show();
         }
         protected Boolean doInBackground(String... params){
             return Networking.login(params);
         }
-        protected void onPostExecute(){
+        protected void onPostExecute(Boolean result){
             dialog.dismiss();
+            Toast msgToast;
+            if(result){
+                msgToast = Toast.makeText(context, "Logged in!", Toast.LENGTH_SHORT);
+            } else {
+                msgToast = Toast.makeText(context, "Login failed.!", Toast.LENGTH_SHORT);
+            }
+            msgToast.show();
         }
     }
 }
--- app/src/main/java/com/hr/techlabapp/Networking.java
+++ app/src/main/java/com/hr/techlabapp/Networking.java
@@ -5,7 +5,11 @@ import android.util.Log;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -17,15 +21,21 @@ import java.util.concurrent.LinkedBlockingDeque;
 public class Networking{
     private static final String TAG = "TL-Networking";
 
-    private static BlockingQueue<JSONObject> queue = new LinkedBlockingDeque<JSONObject>();
+    private static BlockingQueue<JSONObject> inQueue = new LinkedBlockingDeque<JSONObject>();
+    private static BlockingQueue<JSONObject> outQueue = new LinkedBlockingDeque<JSONObject>();
 
     public static Thread thread = new Thread(){
         public void run() {
-            String address = "localhost"; //TODO: How will we even get the right address without hardcoding it?
+            String address = "192.168.178.9"; //TODO: How will we even get the right address without hardcoding it?
+            HttpURLConnection urlConnection;
             try {
                 URL url = new URL("http://" + address + "/");
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoInput(true);
+                urlConnection.setDoOutput(true);
+                urlConnection.connect();
             } catch (MalformedURLException e) {
                 throw new RuntimeException(e);
             } catch (IOException e) {
@@ -34,8 +44,27 @@ public class Networking{
 
             while(true){
                 try{
-                    JSONObject request = queue.take();
+                    JSONObject request = inQueue.take();
+                    DataOutputStream outSteam = new DataOutputStream(urlConnection.getOutputStream());
+                    outSteam.writeBytes(request.toString());
+                    outSteam.flush();
+                    outSteam.close();
+
+                    DataInputStream inStream = new DataInputStream(urlConnection.getInputStream());
+                    BufferedReader d = new BufferedReader(new InputStreamReader(inStream));
+                    StringBuffer sb = new StringBuffer();
+                    String s = "";
+                    while((s = d.readLine()) != null) {
+                        sb.append(s);
+                    }
+                    final JSONObject jsonOut = new JSONObject(sb.toString());
+                    outQueue.add(jsonOut);
+
                 } catch (InterruptedException e){
+                    throw new RuntimeException(e);
+                } catch (IOException e){
+                    throw new RuntimeException(e);
+                } catch (JSONException e){
                     throw new RuntimeException(e);
                 }
 
@@ -44,28 +73,64 @@ public class Networking{
     };
 
     public static boolean login(String... params){
-        long ID = System.currentTimeMillis() / 1000;
+        String ID = String.valueOf(System.currentTimeMillis() / 1000);
 
+        //Hash password
+        String hash;
         try {
+            //Hash password
             MessageDigest md = MessageDigest.getInstance("SHA-512");
             md.update(params[1].getBytes());
-            String hash = md.digest().toString();
-            Log.i(TAG, hash);
+            hash = md.digest().toString();
+        } catch(NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
 
-            JSONObject request = new JSONObject()
+        //Create JSON object
+        JSONObject request;
+        try {
+            request = new JSONObject()
                     .put("requestType", "login")
                     .put("requestID", ID)
                     .put("requestData", new JSONObject()
-                        .put("username", params[0])
-                        .put("password", hash)
+                            .put("username", params[0])
+                            .put("password", hash)
                     );
-            queue.add(request);
 
-        }catch(JSONException e) {
-            throw new RuntimeException(e);
-        }catch(NoSuchAlgorithmException e){
+        } catch (JSONException e){
             throw new RuntimeException(e);
         }
-        return true;
+
+        //Send request & wait for response
+        Boolean loginResult;
+        try{
+            inQueue.add(request);
+            JSONObject response = new JSONObject();
+            Log.i(TAG, "Waiting for login request to complete.");
+            while(true) {
+                response = outQueue.peek();
+                if(response == null){
+                    continue;
+                }
+
+                if ( response.get("requestID").equals(ID)) {
+                    Log.i(TAG, "Received request.");
+                    try {
+                        response = outQueue.take();
+                        break;
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+
+            JSONObject requestData = (JSONObject)response.get("requestData");
+            Log.i(TAG, requestData.toString());
+            loginResult = requestData.getBoolean("loginSuccesful");
+        }catch(JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        return loginResult;
     }
 }

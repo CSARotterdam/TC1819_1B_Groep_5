92b35686bfb37e4df8a2fe450427160bc4f80383
Nolz <36479504+TehNolz@users.noreply.github.com>
2019-04-24 18:58:30 +0200

    Actually use SHA-512 passwords now (lol oops) Rev 2 of app login. Code is now at least 10,000 trillion times more beautiful.

--- app/src/main/java/com/hr/techlabapp/MainActivity.java
+++ app/src/main/java/com/hr/techlabapp/MainActivity.java
@@ -5,13 +5,11 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.hr.techlabapp.Networking.Login;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "TL-MainActivity";
@@ -22,8 +20,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.context = getApplicationContext();
-
-        Networking.thread.start();
     }
 
     public void loginButton(View view){
@@ -31,11 +27,11 @@ public class MainActivity extends AppCompatActivity {
         String username = usernameField.getText().toString();
         EditText passwordField = (EditText)findViewById((R.id.PasswordField));
         String password = passwordField.getText().toString();
-        new Login().execute(username, password);
+        new LoginActivity().execute(username, password);
     }
 
 
-    public class Login extends AsyncTask<String, Void, Boolean>{
+    public class LoginActivity extends AsyncTask<String, Void, Boolean>{
         private ProgressDialog dialog;
 
         protected void onPreExecute(){
@@ -44,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
             dialog.show();
         }
         protected Boolean doInBackground(String... params){
-            return Networking.login(params);
+            return Login.LoginUser(params[0], params[1]);
         }
         protected void onPostExecute(Boolean result){
             dialog.dismiss();
@@ -52,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
             if(result){
                 msgToast = Toast.makeText(context, "Logged in!", Toast.LENGTH_SHORT);
             } else {
-                msgToast = Toast.makeText(context, "Login failed.!", Toast.LENGTH_SHORT);
+                msgToast = Toast.makeText(context, "Login failed!", Toast.LENGTH_SHORT);
             }
             msgToast.show();
         }

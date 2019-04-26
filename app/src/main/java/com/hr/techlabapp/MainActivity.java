1bba83d4ed1093f68c5909d49812822cb749cfee
Nolz <36479504+TehNolz@users.noreply.github.com>
2019-04-26 12:39:22 +0200

    Fix manifest, mainactivity

--- app/src/main/java/com/hr/techlabapp/MainActivity.java
+++ app/src/main/java/com/hr/techlabapp/MainActivity.java
@@ -13,7 +13,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 
-import androidx.navigation.Navigation;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;

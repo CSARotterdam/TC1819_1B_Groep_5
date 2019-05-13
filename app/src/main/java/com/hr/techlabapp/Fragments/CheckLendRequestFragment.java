8926c978d1adfce6b4ab47b6f6c54852bb2460fb
avie0108 <45458508+avie0108@users.noreply.github.com>
2019-05-13 11:57:31 +0200

    Activitys der uit gedefenestreerd

--- app/src/main/java/com/hr/techlabapp/Fragments/CheckLendRequestFragment.java
+++ app/src/main/java/com/hr/techlabapp/Fragments/CheckLendRequestFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +13,6 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.hr.techlabapp.Activitys.ProductInfo;
 import com.hr.techlabapp.Classes.Product;
 import com.hr.techlabapp.CustomViews.UserHistoryDialog;
 import com.hr.techlabapp.R;

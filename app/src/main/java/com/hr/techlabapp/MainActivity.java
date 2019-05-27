e3321370aabf057e9dd4f8a1fb2ab43d1ba5f39d
Saad Eddin Salila <saadeddinsalila@gmail.com>
2019-05-27 02:35:12 +0200

    Help page + language spinner

--- app/src/main/java/com/hr/techlabapp/MainActivity.java
+++ app/src/main/java/com/hr/techlabapp/MainActivity.java
@@ -59,7 +59,10 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.language) {
+            return true;
+        }
+        else if (id == R.id.Help) {
             return true;
         }
 
@@ -79,6 +82,7 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.Notifications) {
 
         } else if (id == R.id.Help) {
+            startActivity(new Intent(MainActivity.this, Help.class));
 
         } else if (id == R.id.Log_out) {
 

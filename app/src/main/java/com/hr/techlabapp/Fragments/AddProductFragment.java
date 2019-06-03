dba49b6a5bc643588cbee892b69cbb5931fbe643
avie0108 <45458508+avie0108@users.noreply.github.com>
2019-06-03 13:54:56 +0200

    prep for merge

--- app/src/main/java/com/hr/techlabapp/Fragments/AddProductFragment.java
+++ app/src/main/java/com/hr/techlabapp/Fragments/AddProductFragment.java
@@ -1,19 +1,36 @@
 package com.hr.techlabapp.Fragments;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import com.hr.techlabapp.Networking.Product;
+import com.hr.techlabapp.Networking.ProductCategory;
 import com.hr.techlabapp.R;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AddProductFragment extends Fragment {
 
+	private Spinner cat;
 
 	public AddProductFragment() {
 		// Required empty public constructor
@@ -27,4 +44,24 @@ public class AddProductFragment extends Fragment {
 		return inflater.inflate(R.layout.fragment_add_product, container, false);
 	}
 
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		cat = getView().findViewById(R.id.product_cat);
+	}
+
+	class getProductCategories extends AsyncTask<Void,Void, List<ProductCategory>>{
+		@Override
+		protected List<ProductCategory> doInBackground(Void... voids) {
+			try{
+				return ProductCategory.getProductCategories(new String[]{"en"});
+			}
+			catch (JSONException ex){
+				return new ArrayList<ProductCategory>();
+			}
+		}
+		@Override
+		protected void onPostExecute(List<ProductCategory> productCategories) {
+		}
+	}
 }

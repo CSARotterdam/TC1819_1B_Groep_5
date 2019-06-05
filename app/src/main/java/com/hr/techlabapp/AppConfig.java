85014b976da6479fad3afe0709991460e7a49976
Spaalone_Babuguuscooties <0965749@hr.nl>
2019-06-05 13:28:14 +0200

    Added a dictionary of host IPs Added language string. Set automatically to current locale language.

--- app/src/main/java/com/hr/techlabapp/AppConfig.java
+++ app/src/main/java/com/hr/techlabapp/AppConfig.java
@@ -2,6 +2,16 @@ package com.hr.techlabapp;
 
 import com.hr.techlabapp.Networking.*;
 
-final class AppConfig {
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public final class AppConfig {
+	public static final Map<String, String> serverAddresses = new HashMap<String, String>()
+	{{
+		put("Conor HR", "145.137.52.171");
+	}};
+
+	public static String language = Locale.getDefault().getLanguage();
 	public static User currentUser;
 }

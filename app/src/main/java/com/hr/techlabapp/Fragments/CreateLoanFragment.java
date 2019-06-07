1a8d6fc94b8693bcc5fdcedf91fa32abe2b7f2da
Nolz <36479504+TehNolz@users.noreply.github.com>
2019-06-07 19:30:07 +0200

    Build error fixes

--- app/src/main/java/com/hr/techlabapp/Fragments/CreateLoanFragment.java
+++ app/src/main/java/com/hr/techlabapp/Fragments/CreateLoanFragment.java
@@ -4,15 +4,16 @@ import android.graphics.Bitmap;
 import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.hr.techlabapp.Networking.Exceptions;
 import com.hr.techlabapp.Networking.LoanItem;

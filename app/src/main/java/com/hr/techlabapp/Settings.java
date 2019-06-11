5e3898b2b4d83dc1d76ba71c3c37836f42361e92
avie0108 <45458508+avie0108@users.noreply.github.com>
2019-06-11 10:15:14 +0200

    made <@489042529908686861>(saad) shit fragments

--- app/src/main/java/com/hr/techlabapp/Settings.java
+++ app/src/main/java/com/hr/techlabapp/Settings.java
@@ -1,19 +1,25 @@
 package com.hr.techlabapp;
 
 import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
-public class Settings extends AppCompatActivity {
+public class Settings extends Fragment {
     Button language;
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.settings, container, false);
     }
-
 }
